d60b0c56d4870571e45e523d66244bdc20b4d626
thenatural86 <philip.smalls86@gmail.com>
2025-02-18 17:41:32 -0500

    classes 1

---
+++ ruby_classes/GoodDog.rb
@@ -0,0 +1,23 @@
+class GoodDog
+  attr_accessor :name, :height, :weight
+
+  def initialize(n, h, w)
+    @name = n
+    @height = h
+    @weight = w
+  end
+
+  def speak
+    "#{@name} says Arf!"
+  end
+
+  def change_info(n, h, w)
+    self.name = n
+    self.height = h
+    self.weight = w
+  end
+
+  def info
+    "#{name} weighs #{weight} and is #{height} tall."
+  end
+end
