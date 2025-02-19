064e0307c2e6b51867246045ecd7762bfe9307c1
thenatural86 <philip.smalls86@gmail.com>
2025-02-18 22:48:36 -0500

    inheritance exercises

---
+++ ruby_classes/vehicle.rb
@@ -0,0 +1,13 @@
+class Vehicle
+  def self.gas_mileage(gallons, miles)
+    puts "#{miles / gallons} miles per gallon of gas"
+  end
+end
+
+class MyCar < Vehicle
+  NUMBER_OF_DOORS = 4
+end
+
+class MyTruck < Vehicle
+  NUMBER_OF_DOORS = 2
+end
