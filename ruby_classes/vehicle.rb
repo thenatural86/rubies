63a3cb48d89cfe5c0c48f8d1ea18c855214a84d9
thenatural86 <philip.smalls86@gmail.com>
2025-02-19 01:46:23 -0500

    refactor car class

--- ruby_classes/vehicle.rb
+++ ruby_classes/vehicle.rb
@@ -6,7 +6,9 @@ end
 
 
 class Vehicle
-  @@number_of_vehicles = 0
+  @@number_of_vehicles += 1
+  attr_accessor :color
+  attr_reader :year
 
   def self.gas_mileage(gallons, miles)
     puts "#{miles / gallons} miles per gallon of gas"
@@ -17,15 +19,67 @@ class Vehicle
   end
 
   def initialize
-    @@number_of_vehicles += 1
+    @year = year
+    @color = color
+    @model = model
+    @current_speed = 0
+  end
+
+  def speed_up(number)
+    @current_speed += number
+    puts "You push the gas and accelerate #{number} mph."
+  end
+
+  def brake(number)
+    @current_speed -= number
+    puts "You push the brake and decelerate #{number} mph."
+  end
+
+  def current_speed
+    puts "You are now going #{@current_speed} mph."
+  end
+
+  def shut_down
+    @current_speed = 0
+    puts "Let's park this bad boy!"
+  end
+
+  def spray_paint(color)
+    self.color = color
+    puts "Your new #{color} paint job looks great!"
+  end
+end
+class MyTruck < Vehicle
+  include Towable
+  NUMBER_OF_DOORS = 2
+
+  def to_s
+    "My truck is a #{self.color}, #{self.year}, #{self.model}!"
   end
 end
 
 class MyCar < Vehicle
   NUMBER_OF_DOORS = 4
+
+  def to_s
+    "My car is a #{self.color}, #{self.year}, #{self.model}!"
+  end
 end
 
-class MyTruck < Vehicle
-  include Towable
-  NUMBER_OF_DOORS = 2
-end
+puts Vehicle.ancestors
+puts MyCar.ancestors
+puts MyTruck.ancestors
+
+
+lumina = MyCar.new(1997, 'chevy lumina', 'white')
+lumina.speed_up(20)
+lumina.current_speed
+lumina.speed_up(20)
+lumina.current_speed
+lumina.brake(20)
+lumina.current_speed
+lumina.brake(20)
+lumina.current_speed
+lumina.shut_down
+lumina.current_speed
+lumina.spray_paint('red')
