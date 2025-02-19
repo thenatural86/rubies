a19f1f9f3778f44cdd5aaefe6e1fceca376e16c0
thenatural86 <philip.smalls86@gmail.com>
2025-02-18 22:58:20 -0500

    adds class method

--- ruby_classes/vehicle.rb
+++ ruby_classes/vehicle.rb
@@ -1,6 +1,16 @@
 class Vehicle
+  @@number_of_vehicles = 0
+
   def self.gas_mileage(gallons, miles)
     puts "#{miles / gallons} miles per gallon of gas"
+  end
+
+  def self.number_of_vehicles
+    puts "This program has created #{@@number_of_objects_created} vehicles"
+  end
+
+  def initialize
+    @@number_of_vehicles += 1
   end
 end
 
